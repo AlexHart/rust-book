0bd8110f9d0c7b151e7e6042a47364116cac0adc
Alejandro Hartlohner Utrera <hartformer@hotmail.com>
2021-05-06 22:25:00 +0200

    Add more slice examples

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -11,6 +11,25 @@ fn main() {
 
     let sw = second_word(&s);
     println!("Second word: {}", sw);
+
+    // Vec slices examples
+    let nums: Vec<i32> = vec!(1, 2, 3, 45, 234, 123);
+    let nums_slice = &nums[2..4];
+    print_nums_slice(nums_slice);
+    
+    let nums_slice = &nums[3..];
+    print_nums_slice(nums_slice);
+
+    let nums_slice = &nums[..5];
+    print_nums_slice(nums_slice);
+}
+
+fn print_nums_slice(nums_slice: &[i32]){
+    println!("--------------------------------");
+    println!("Print slice of numbers with length: {}", nums_slice.len());
+    for num in nums_slice {
+        println!("Num in slice: {}", num);
+    }
 }
 
 fn first_word(s: &str) -> &str {
