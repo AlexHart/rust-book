dc230dd90f82c1553f8aff5cffc14d6820927af7
Alejandro Hartlohner Utrera <hartformer@hotmail.com>
2021-05-15 01:05:16 +0200

    Add channels example

---
+++ message_passing/src/main.rs
@@ -0,0 +1,107 @@
+use std::time::Duration;
+use std::sync::mpsc;
+use std::thread;
+use std::sync::mpsc::TryRecvError::{Empty, Disconnected};
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        for i in 1..11 {
+            let val = format!("{} - Hello World!", i);
+            match tx.send(val) {
+                Ok(_) => (),
+                Err(err) => { 
+                    println!("Error sending {}", err);
+                    panic!("Error sending, kill app");
+                }
+            }
+        }
+    });
+
+    // Blocking receive
+    // loop {
+    //     match rx.recv() {
+    //         Ok(received) => println!("Got: {}", received),
+    //         Err(err) => {
+    //             println!("Error: {}", err);
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // try_recv
+    loop {
+        match rx.try_recv() {
+            Ok(received) => println!("Got {}", received),
+            Err(e) => {
+                match e {
+                    Empty => continue,
+                    Disconnected => break
+                }
+            }
+        }
+        thread::sleep(Duration::from_millis(64));
+    }
+
+    println!();
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+    println!();
+
+    println!("Multiple producers example");
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+}
