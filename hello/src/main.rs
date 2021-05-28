15d7e0371c99f05961c4d5f8e132a78e1776cfe3
Alejandro Hartlohner Utrera <hartformer@hotmail.com>
2021-05-28 22:42:33 +0200

    Graceful shutdown and cleanup

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -22,7 +22,7 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
